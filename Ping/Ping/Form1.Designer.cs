72dbb82ec0224d48ae372544f35307824438c95d
VC Science Labs <vscilab@vcccd.edu>
2018-03-14 14:27:11 -0700

    added Ping Pong game

---
+++ Ping/Ping/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Ping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.background = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.points_lbl = new System.Windows.Forms.Label();
+            this.Score = new System.Windows.Forms.Label();
+            this.pong = new System.Windows.Forms.PictureBox();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.background.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // background
+            // 
+            this.background.Controls.Add(this.label2);
+            this.background.Controls.Add(this.label1);
+            this.background.Controls.Add(this.points_lbl);
+            this.background.Controls.Add(this.Score);
+            this.background.Controls.Add(this.pong);
+            this.background.Controls.Add(this.racket);
+            this.background.Location = new System.Drawing.Point(2, -1);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(997, 572);
+            this.background.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(842, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "REPLAY - F1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(865, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "EXIT - esc";
+            // 
+            // points_lbl
+            // 
+            this.points_lbl.AutoSize = true;
+            this.points_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.points_lbl.Location = new System.Drawing.Point(83, 18);
+            this.points_lbl.Name = "points_lbl";
+            this.points_lbl.Size = new System.Drawing.Size(16, 16);
+            this.points_lbl.TabIndex = 3;
+            this.points_lbl.Text = "0";
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score.Location = new System.Drawing.Point(10, 14);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(79, 22);
+            this.Score.TabIndex = 2;
+            this.Score.Text = "SCORE: ";
+            // 
+            // pong
+            // 
+            this.pong.BackColor = System.Drawing.Color.Red;
+            this.pong.Location = new System.Drawing.Point(391, 45);
+            this.pong.Name = "pong";
+            this.pong.Size = new System.Drawing.Size(25, 25);
+            this.pong.TabIndex = 1;
+            this.pong.TabStop = false;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.Color.Black;
+            this.racket.Location = new System.Drawing.Point(317, 494);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(200, 25);
+            this.racket.TabIndex = 0;
+            this.racket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 569);
+            this.Controls.Add(this.background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.background.ResumeLayout(false);
+            this.background.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel background;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.PictureBox pong;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label points_lbl;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Ping/Ping/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ping
+{
+    public partial class Form1 : Form
+    {
+        public 
+        int speedball = 4; //speed of ball
+        int speed_top = 4;
+        int pts = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            this.FormBorderStyle = FormBorderStyle.None; // removes any border
+            this.TopMost = true; //brings the form to the front
+
+
+            racket.Top = background.Bottom - (background.Bottom/10);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            racket.Left = Cursor.Position.X - (racket.Width/2);
+            pong.Left += speedball; //move the pong
+            pong.Top += speed_top;
+
+            if(pong.Bottom >= racket.Top && 
+                pong.Bottom <= racket.Bottom && 
+                pong.Left >= racket.Left && 
+                pong.Right <= racket.Right )
+            {
+                speed_top += 2;
+                speedball += 2;
+                speed_top = -speed_top;
+                pts += 1;
+                points_lbl.Text = pts.ToString();
+
+                Random r = new Random();
+                background.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+                
+            }
+            if (pong.Left <= background.Left) speedball = -speedball;
+            if (pong.Right >= background.Right) speedball = -speedball;
+            if (pong.Top <= background.Top) speed_top = -speed_top;
+            if (pong.Bottom >= background.Bottom) timer1.Enabled = false; //ends the game
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) this.Close(); //closes the program
+            if (e.KeyCode == Keys.F1)
+            {
+                pong.Left = 50;
+                pong.Top = 50;
+                speedball = 4;
+                speed_top = 4;
+                points_lbl.Text = "0";
+                pts = 0;
+                timer1.Enabled = true;
+
+            }
+        }
+
+      
+    }
+}
